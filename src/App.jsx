5087569d242469e56f0501a3f25752742b4085fa
Raya Aliyeva <73crd54@code.edu.az>
2026-07-20 23:26:10 +0400

    folder name changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import AuthRoute from "./utils/AuthRoute";
-import PrivateRoute from "./utils/PrivateRoute";
+import AuthRoute from "./routers/AuthRoute";
+import PrivateRoute from "./routers/PrivateRoute";
 import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AuthRoute user={user} />}>
-            <Route path="/login" element={<Login setUser={setUser} />} />
-          </Route>
-          <Route element={<PrivateRoute user={user} />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthRoute user={user} />}>
+          <Route path="/login" element={<Login setUser={setUser} />} />
+        </Route>
+        <Route element={<PrivateRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
